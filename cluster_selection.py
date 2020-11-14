938ac90d70f27814df34655a7566410b804ad5f8
Jorge <jsamo25@gmail.com>
2020-11-14 21:35:07 +0100

    Kmeans clustering for word2vec inputs, and PCA (2d)

--- cluster_selection.py
+++ cluster_selection.py
@@ -11,6 +11,10 @@ from gensim.models import Word2Vec
 from sklearn import metrics
 from pdb import set_trace
 
+"""********************************************************
+                    data instantiation
+********************************************************"""
+
 data = pd.read_csv("data/data.csv")
 stopwords = stopwords.words()
 
@@ -28,9 +32,21 @@ def clean_sentence(context):
 data["text"] = data["text"].apply(clean_sentence)
 corpus = [sentence.split() for sentence in data["text"]]
 
-model = Word2Vec(corpus,min_count=3)
+
+"""********************************************************
+                    K means model training
+********************************************************"""
+model = Word2Vec(corpus,min_count=10)
 vocabulary = model.wv.vocab
 X = model[vocabulary]
+K = 5
+kmeans_model = KMeans(n_clusters=K).fit(X)
+cluster_centers = kmeans_model.cluster_centers_
+
+
+"""********************************************************
+                    dimension reduction 
+********************************************************"""
 
 pca = PCA(n_components=2)
 result = pca.fit_transform(X)
@@ -39,17 +55,20 @@ x1 = np.array(result[:,0])
 x2 = np.array(result[:,1])
 
 # create new plot and data
-plt.plot()
 X = np.array(list(zip(x1, x2))).reshape(len(x1), 2)
-colors = ['b', 'g', 'c']
-markers = ['o', 'v', 's']
 
-# KMeans algorithm
-K = 3
+"""********************************************************
+                    2-d scatter plot
+********************************************************"""
+
+# KMeans algorithm for 2-d
 kmeans_model = KMeans(n_clusters=K).fit(X)
-
-print(kmeans_model.cluster_centers_)
 centers = np.array(kmeans_model.cluster_centers_)
+print("2d centers", centers)
+
+
+colors = ["b", "g", "r", "c", "m"]
+markers = ['o', 'v', 's', "*", "h"]
 
 plt.plot()
 plt.title('k means centroids')
@@ -57,13 +76,12 @@ plt.title('k means centroids')
 for i, l in enumerate(kmeans_model.labels_):
     plt.plot(x1[i], x2[i], color=colors[l], marker=markers[l],ls='None')
 
+plt.scatter(result[:,0],result[:,1])
 plt.scatter(centers[:,0], centers[:,1], marker="x", color='r')
 
-
-
-plt.scatter(result[:,0],result[:,1])
-words = list(model.wv.vocab)
+words = list(vocabulary)
 for i, word in enumerate(words):
     plt.annotate(word, xy=(result[i,0], result[i,1]))
 
 plt.show()
+set_trace()
